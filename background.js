e41e3e0c5e118b5dac33372e18abb5cd1995c49f
rajeev-k47 <rajeevkumawat2007@outlook.com>
2024-11-29 19:36:56 +0530

    <*/*>?

--- background.js
+++ background.js
@@ -18,7 +18,7 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   console.log('Keypressed:',message.key);
   if (message.key === '.') {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.create({url: "https://localhost:8000"});
+      chrome.tabs.create({url: "http://localhost:8000"});
     });
   }
   if (message.key === 'Alt') {
